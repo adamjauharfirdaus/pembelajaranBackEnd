93b64f95ef59583dee36806fc3e365873eb10182
Nura21 <m.irfansyafalah@gmail.com>
2025-11-02 19:23:34 +0700

    komit2

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "=== PERKENALKAN !!! === \n \n";
+echo "\n=== PERKENALAN === \n \n";
 function namaSaya() {
     echo "Adam Jauhar Firdaus";
 }
@@ -8,7 +8,7 @@ $umur = 19;
 $mahasiswa = "Teknik Informatika Universitas Pamulang \n";
 echo "halo, nama saya ", namaSaya(), "\numur saya ", $umur, "\nsaat ini saya merupakan mahasiswa ", $mahasiswa;
 
-echo "\n == OPERASI ARITMATIKA SEDERHANA ===\n";
+echo "\n=== OPERASI ARITMATIKA SEDERHANA ===\n";
 function tambah($a, $b) {
     return $a + $b;
 }
@@ -34,11 +34,11 @@ echo "tentukan nilai dari nilai variable b : ";
 $angka2 = trim(fgets(STDIN));
 
 
-echo "Hasil pertambahan dari ",$angka1 ,"dan", $angka2 , "adalah : ", tambah($angka1, $angka2) , "\n";
-echo "Hasil pengurangan dari ",$angka1 ,"dan", $angka2 , "adalah : ", kurang($angka1, $angka2) , "\n";
-echo "Hasil pembagian dari ",$angka1 ,"dan", $angka2 , "adalah : ", kali($angka1, $angka2) , "\n";
+echo "\nHasil pertambahan dari angka ",$angka1 ," dan ", $angka2 , " adalah : ", tambah($angka1, $angka2) , "\n";
+echo "Hasil pengurangan dari angka ",$angka1 ," dan ", $angka2 , " adalah : ", kurang($angka1, $angka2) , "\n";
+echo "Hasil pembagian dari angka ",$angka1 ," dan ", $angka2 , " adalah : ", kali($angka1, $angka2) , "\n";
 if ($angka2 != 0 ) {
-    echo "Hasil pembagian dari ",$angka1 ,"dan", $angka2 , "adalah : ", bagi($angka1, $angka2) , "\n";
+    echo "Hasil pembagian dari angka ",$angka1 ," dan ", $angka2 , " adalah : ", bagi($angka1, $angka2) , "\n";
 } else {
      echo "pembagian dengan angka 0 tidak diperbolehkan ! \n";
 }
@@ -47,8 +47,8 @@ if ($angka2 != 0 ) {
 
 
 
-echo "=== PERHITUNGAN NILAI MATAKULIAH (NILAI DIBAWAH 75 WAJIB REMEDIAL ===";
-echo "berapa nilai mata kuliah algoritma pemrograman kamu? : ";
+echo "\n=== PERHITUNGAN NILAI MATAKULIAH (NILAI DIBAWAH 75 WAJIB REMEDIAL ===\n";
+echo "\nberapa nilai mata kuliah algoritma pemrograman kamu? : ";
 
 $nilai_algo = trim(fgets(STDIN));
 if ($nilai_algo >= 75) {
@@ -65,14 +65,14 @@ do {
 } while ($deret < 21);
 
 
-echo "ketikan nilai untuk mengecek apakah nilai untuk variable a : ";
+echo "\nketikan nilai untuk mengecek apakah nilai untuk variable a : ";
 
 $var_a = trim(fgets(STDIN));
 $hasil_a = $var_a % 2;
 if ($hasil_a == 0) {
-    echo "bilangan termasuk genap";
+    echo "\nbilangan termasuk genap";
 } else {
-    echo "bilangan termasuk ganjil";
+    echo "\nbilangan termasuk ganjil";
 }
 
 
